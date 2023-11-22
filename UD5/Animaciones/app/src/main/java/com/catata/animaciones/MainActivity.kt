f062644f1c0fe546bf3ddfaaa5ea38fe5f2a263d
sdram58 <sdram58@hotmail.com>
2023-11-22 14:03:21 +0100

    Animations

--- UD5/Animaciones/app/src/main/java/com/catata/animaciones/MainActivity.kt
+++ UD5/Animaciones/app/src/main/java/com/catata/animaciones/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOut
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -46,9 +47,10 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.TextButton
 import androidx.compose.ui.text.style.TextAlign
 import com.catata.animaciones.ui.theme.AnimacionesTheme
@@ -64,29 +66,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Content()
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by rememberSaveable {
+fun Content() {
+    /*var expanded by rememberSaveable {
         mutableStateOf(false)
     }
     Column(
-        verticalArrangement = Arrangement.Top,
         modifier = Modifier
             .fillMaxWidth()
-            /*.animateContentSize(animationSpec = tween(
-                durationMillis = 2000,
-                easing = LinearEasing
-            ))*/
-            .background(Color(0xFFFFA0C0))
-            //.padding(10.dp)
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 2000,
+                    easing = LinearEasing
+                )
+            )
 
     ) {
         Row(
@@ -104,7 +104,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             }
         }
 
-        Text(text = "Hey!")
         if(expanded){
             Spacer(modifier = Modifier.height(8.dp))
 
@@ -128,6 +127,206 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     "altura del mismo creador de todo el universo.",
                  textAlign = TextAlign.Justify)
         }
+    }*/
+
+/*
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(25.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+
+        var animateColor by rememberSaveable { mutableStateOf(false) }
+        val backgroundColor by animateColorAsState(if (animateColor) Color(0xFFFFA020) else Color(0xFF40C0FF))
+        Button(onClick = { animateColor = !animateColor }) {
+            Text(text = "Cambiar color")
+        }
+        Text(
+            text = "Hola Rick!",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .background(backgroundColor)
+                .padding(20.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var animateAlpha by rememberSaveable { mutableStateOf(true) }
+        val alpha: Float by animateFloatAsState(if (animateAlpha) 1f else 0.3f)
+        Button(onClick = { animateAlpha = !animateAlpha }) {
+            Text(text = "Cambiar opacidad")
+        }
+        Box(
+            modifier = Modifier
+                .graphicsLayer(alpha = alpha)
+                .fillMaxWidth()
+                .background(Color(0xFFFFA020))
+        ) {
+            Text(
+                text = "Hola Rick!",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(20.dp)
+
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var visible by rememberSaveable { mutableStateOf(false) }
+        Button(onClick = {
+            visible = !visible
+        }) {
+            Text(text = if (visible) "Ocultar" else "Mostrar")
+        }
+        AnimatedVisibility(visible = visible) {
+            Text(
+                text = "Hola Rick!",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(Color(0xFFFFA020))
+                    .padding(20.dp)
+                    .fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var times by rememberSaveable { mutableStateOf(0) }
+        Row {
+            Button(onClick = { times++ }) {
+                Text("+1")
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(onClick = { times-- }) {
+                Text("-1")
+            }
+
+            AnimatedContent(targetState = times, label = "") {
+                Text(
+                    text = "Cuenta: $it",
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+
+            AnimatedContent(
+                targetState = times,
+                // Personalizando la animación
+                transitionSpec = {
+                    // targetState: nuevo valor de la variable de estado
+                    // initialState: valor anterior de la variable de estado
+                    if (targetState > initialState) {
+                        (slideInVertically { height -> height } + fadeIn())
+                            .togetherWith(slideOutVertically { height -> -height } + fadeOut())
+                    } else {
+                        (slideInVertically { height -> -height } + fadeIn())
+                            .togetherWith(slideOutVertically { height -> height } + fadeOut())
+                    }
+                },
+                label = ""
+            ) { targetCount ->
+                Text(
+                    text = "$targetCount",
+                    modifier = Modifier.padding(10.dp)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        val sections = arrayOf("Anime", "Manga", "Otros")
+        var visibleContent by rememberSaveable { mutableStateOf(sections[0]) }
+        Row {
+            for (section in sections) {
+                Button(onClick = {
+                    visibleContent = section
+                }) {
+                    Text(text = section)
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+            }
+        }
+        Crossfade(
+            targetState = visibleContent,
+            label = "",
+        ) { screen ->
+            when (screen) {
+                "Manga" -> Text("Mostrar Manga")
+                "Anime" -> Text("Mostrar Anime")
+                "Otros" -> Text("Mostrar otros")
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        var expanded by rememberSaveable { mutableStateOf(false) }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .animateContentSize(
+                    animationSpec = tween(
+                        durationMillis = 2000,
+                        easing = LinearEasing
+                    )
+                )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Son goku")
+                TextButton(onClick = { expanded = !expanded }) {
+                    Text(if (expanded) "Menos." else "Leer más...")
+                }
+            }
+            if (expanded) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = "Es el protagonista de la serie de manga y anime Dragon Ball." +
+                            " Fue creado por Akira Toriyama en 1984. Al comienzo de la" +
+                            " historia, Goku aparece como un niño que practica las artes" +
+                            " marciales y que posee una cola de mono y una fuerza sobrehumana," +
+                            " pero más adelante se revela que es un extraterrestre de la raza" +
+                            " ficticia saiyajin, y que su nombre original es Kakarotto",
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+    }*/
+
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween(
+                    durationMillis = 2000,
+                    easing = LinearEasing
+                )
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Son goku")
+            TextButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded) "Menos." else "Leer más...")
+            }
+        }
+        if (expanded) {
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Es el protagonista de la serie de manga y anime Dragon Ball." +
+                        " Fue creado por Akira Toriyama en 1984. Al comienzo de la" +
+                        " historia, Goku aparece como un niño que practica las artes" +
+                        " marciales y que posee una cola de mono y una fuerza sobrehumana," +
+                        " pero más adelante se revela que es un extraterrestre de la raza" +
+                        " ficticia saiyajin, y que su nombre original es Kakarotto",
+                textAlign = TextAlign.Justify
+            )
+        }
     }
 
     
@@ -135,8 +334,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ContentPreview() {
     AnimacionesTheme {
-        Greeting("Android")
+        Content()
     }
 }
