7b0f6f149eccfad41a04ae7d982e56a6e22a3800
sdram58 <sdram58@hotmail.com>
2023-11-29 13:33:32 +0100

    Updated Animation example

--- UD5/Animaciones/app/build.gradle.kts
+++ UD5/Animaciones/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    //implementation("androidx.compose.animation:animation-android:1.5.4")
+    implementation("androidx.compose.animation:animation-android:1.5.4")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
 
